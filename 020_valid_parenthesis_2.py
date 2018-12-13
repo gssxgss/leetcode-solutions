8d231af853ab468583a2aac94640aec79a39a29d
GSSxGSS <gsshcl@gmail.com>
2018-12-13 22:38:24 +0900

    forgot to commit

--- 020_valid_parenthesis_2.py
+++ 020_valid_parenthesis_2.py
@@ -16,18 +16,22 @@ class Solution:
         stack_len = len(s) // 2
         stack = [None] * stack_len
         index = 0
-        for i in range(len(s)):
-            current_char = s[i]
-            if current_char in ref:
+        for c in s:
+            if c in ref:
                 if not index:
                     return False
                 index -= 1
                 last_char = stack[index]
-                if not last_char or last_char != ref[current_char]:
+                if not last_char or last_char != ref[c]:
                     return False
             else:
                 if index > stack_len - 1:
                     return False
-                stack[index] = current_char
+                stack[index] = c
                 index += 1
         return not index
+
+
+print(Solution().isValid('[[]]()'))
+print(Solution().isValid(']()'))
+print(Solution().isValid('(('))
