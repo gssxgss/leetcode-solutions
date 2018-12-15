226006e2d86b7e63ba481ae469e8d92aea3fe285
GSSxGSS <gsshcl@gmail.com>
2018-12-15 22:36:49 +0900

    070 as a fibonacci problem

--- python3/070_climbing_stairs.py
+++ python3/070_climbing_stairs.py
@@ -1,10 +1,27 @@
 # https://leetcode.com/problems/climbing-stairs/
+# Hint: https://leetcode.com/problems/climbing-stairs/discuss/25299/Basically-it's-a-fibonacci.
 
 
 class Solution:
+
+    def __init__(self):
+        self.cache = {}
+
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        pass
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        if n in self.cache:
+            return self.cache[n]
+        else:
+            res = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            self.cache[n] = res
+            return res
