c48f2ae5104dbfb9aa8f9c0087398d7252e6d526
GSSxGSS <gsshcl@gmail.com>
2018-12-15 16:44:49 +0900

    211 java version done

---
+++ java/src/Problem211.java
@@ -0,0 +1,119 @@
+// https://leetcode.com/problems/add-and-search-word-data-structure-design/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem211 {
+
+    public static void main(String[] args) {
+        System.out.println("211. Add and Search Word - Data structure design");
+    }
+}
+
+class TrieNode {
+
+    private Map<Character, TrieNode> children;
+    private boolean isFinish;
+
+    /**
+     * Initialize Trie Node
+     */
+    TrieNode() {
+        this.isFinish = false;
+        this.children = new HashMap<>();
+    }
+
+    /**
+     * Add Word to TrieNode
+     *
+     * @param word String to add
+     */
+    void addWord(String word) {
+        if (word.length() == 1) {
+            char head = word.charAt(0);
+            TrieNode node = setTrieNode(head);
+            node.isFinish = true;
+
+        } else {
+            char head = word.charAt(0);
+            String rest = word.substring(1);
+            TrieNode node = setTrieNode(head);
+            node.addWord(rest);
+        }
+    }
+
+    /**
+     * Get or Create a TrieNode
+     *
+     * @param character A character in the word
+     * @return A TrieNode
+     */
+    private TrieNode setTrieNode(char character) {
+        TrieNode node;
+        if (this.children.containsKey(character)) {
+            node = this.children.get(character);
+        } else {
+            node = new TrieNode();
+            this.children.put(character, node);
+        }
+        return node;
+    }
+
+    /**
+     * Search if character in TrieNode.children
+     *
+     * @param string A string to search
+     * @return if string in children
+     */
+    boolean search(String string) {
+        if (string.length() == 1) {
+            char character = string.charAt(0);
+            if (character == '.') {
+                for (TrieNode node : this.children.values()) {
+                    if (node.isFinish) return true;
+                }
+                return false;
+            }
+            return this.children.containsKey(character) && this.children.get(character).isFinish;
+        }
+
+        char head = string.charAt(0);
+        String rest = string.substring(1);
+        if (head == '.') {
+            for (TrieNode node : this.children.values()) {
+                if (node.search(rest)) return true;
+            }
+            return false;
+        }
+        return this.children.containsKey(head) && this.children.get(head).search(rest);
+
+    }
+}
+
+class WordDictionary {
+
+    private TrieNode root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+        if (word.length() == 0) return;
+        this.root.addWord(word);
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        if (word.length() == 0) return false;
+        return this.root.search(word);
+    }
+}
---
+++ java/test/Problem211Test.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem211Test {
+    @Test
+    void testAddWord() {
+        WordDictionary dict = new WordDictionary();
+        dict.addWord("cat");
+        assertTrue(dict.search("cat"));
+    }
+
+    @Test
+    void testFuzzySearch() {
+        WordDictionary dict = new WordDictionary();
+        dict.addWord("cat");
+        dict.addWord("caterpillar");
+        assertTrue(dict.search("c.."));
+        assertTrue(dict.search("..t"));
+        assertTrue(dict.search("..."));
+        assertFalse(dict.search("cat.."));
+        assertFalse(dict.search(""));
+    }
+
+    @Test
+    void leetCodeTest() {
+        WordDictionary dict = new WordDictionary();
+        dict.addWord("bad");
+        dict.addWord("dad");
+        dict.addWord("mad");
+        assertFalse(dict.search("pad"));
+        assertTrue(dict.search("bad"));
+        assertTrue(dict.search(".ad"));
+        assertTrue(dict.search("b.."));
+    }
+}
