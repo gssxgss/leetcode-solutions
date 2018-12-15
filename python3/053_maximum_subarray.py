edead51924ee3ea617c5f1457508ba27a2cae9a7
GSSxGSS <gsshcl@gmail.com>
2018-12-15 21:20:08 +0900

    convert java to python

--- python3/053_maximum_subarray.py
+++ python3/053_maximum_subarray.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/maximum-subarray/
-
+# Kanade's Algorithm
 
 class Solution:
     def maxSubArray(self, nums):
@@ -7,4 +7,9 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        pass
+        max_sofar = max_ending_here = nums[0]
+        for i in range(1, len(nums)):
+            max_ending_here = max(max_ending_here + nums[i], nums[i])
+            max_sofar = max(max_sofar, max_ending_here)
+        return max_sofar
+
